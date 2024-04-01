deb4023bfc3672e1cb2a681c7bf12a25fc30325c
manal <manal.ahmadi@uir.ac.ma>
2024-04-01 09:47:09 -0500

    add-video feature

--- eventproject/settings.py
+++ eventproject/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3003",  # React's default local dev server address
+    "http://localhost:3001",  # React's default local dev server address
 ]
 
 
